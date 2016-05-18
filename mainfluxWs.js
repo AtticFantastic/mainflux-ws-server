690097c061e1f1b1cb4b774e7c606ea7a369561a
Drasko DRASKOVIC <drasko.draskovic@gmail.com>
2016-05-19 00:47:21 +0200

    Correct JSON strings
    Signed-off-by: Drasko DRASKOVIC <drasko.draskovic@gmail.com>

--- mainfluxWs.js
+++ mainfluxWs.js
@@ -18,6 +18,13 @@ wsServer.on('connection', function(ws) {
     ws.on('message', function(msg) {
         console.log('received: %s', msg);
 
+        console.log(typeof msg);
+
+        var jmsg = JSON.parse(msg);
+
+        console.log(typeof jmsg);
+        console.log(jmsg);
+
         /**
          * WS client can subscribe on per device subject
          * or just send commands to Core via NATS (ex. `getDevices`)
@@ -25,14 +32,20 @@ wsServer.on('connection', function(ws) {
         if (jmsg.method === 'subscribe') {
             console.log('SUBSCRIBING...');
 
-            nats.subscribe('devices.' + msg.body.deviceId + '.attributes.' + msg.body.attribute, function(upd) {
+            //nats.subscribe('devices.' + msg.body.deviceId, function(upd) {
+            nats.subscribe('core_out', function(upd) {
                 console.log('Received a message: ' + upd);
-                /** Recieved message from NATS is string type, we need to convert it to JSON */
-                ws.send(JSON.parse(upd));
+                /** Recieved message from NATS is string type, we can send it right away to WS */
+                ws.send(upd);
             });
         } else {
             nats.request('core_in', JSON.stringify(msg), {'max':1}, function(rsp) {
-                ws.send(JSON.parse(rsp));
+                /**
+                 * WS is a stream - only accepts strings, not JSON objects.
+                 * But that is OK, messages that arrive from NATS are pure strings,
+                 * we can send them directly to WS.
+                 */
+                ws.send(rsp);
             });
         }
     });
